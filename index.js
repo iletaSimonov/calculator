9ab8c351170e6fb6d7da9d6e812cd8a6997e5185
Ileta Simonov <ileta.simonov@yahoo.com>
2023-01-18 19:14:51 -0800

    adds a base to the calculator web page. WIP

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+add = function(number1, number2) {
+  return number1 + number2;
+}
+
+subtract = function(number1, number2) {
+  return number1 - number2;
+}
+
+multiply = function(number1, number2) {
+  return number1 * number2;
+}
+
+divide = function(number1, number2) {
+  return number1 / number2;
+}
+
